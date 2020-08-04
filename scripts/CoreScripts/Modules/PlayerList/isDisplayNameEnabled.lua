bb21298c1065f31aacda4a5f31045ca0b63a45ea
Roblox Client Tracker <rbxclientwatch@gmail.com>
2020-08-04 01:32:12 -0500

    0.443.0.409485 (Scripts)

--- scripts/CoreScripts/Modules/PlayerList/isDisplayNameEnabled.lua
+++ scripts/CoreScripts/Modules/PlayerList/isDisplayNameEnabled.lua
@@ -5,10 +5,15 @@ local PolicyService = require(RobloxGui.Modules.Common.PolicyService)
 
 local FFlagPlayerListUseDisplayName = game:DefineFastFlag("PlayerListUseDisplayName", false)
 local FFlagPlayerListUseDisplayNameChina = game:DefineFastFlag("PlayerListUseDisplayNameChina", false)
+local FFlagPlayerListUseDisplayNameChina2 = game:DefineFastFlag("PlayerListUseDisplayNameChina2", false)
 
 local function isDisplayNameEnabled()
 	if FFlagPlayerListUseDisplayName then
 		return true
+	end
+
+	if FFlagPlayerListUseDisplayNameChina2 then
+		return PolicyService:IsSubjectToChinaPolicies()
 	end
 
 	if FFlagPlayerListUseDisplayNameChina then
